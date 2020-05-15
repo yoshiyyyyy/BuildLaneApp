c3204706f65f91cfc33edc08e5bcef79646b70b5
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2020-05-15 17:01:43 +0900

    initialcommit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,request,redirect,flash,send_from_directory
+from werkzeug.utils import secure_filename
+import requests
+import os
+import sys
+# sys.path.append("myspa")
+# import BuildLane
+
+
+
+UPLOAD_FOLDER = '/Users/yoshimasa/projects/myspa/backend/uploads/'
+
+app = Flask(__name__, template_folder='./templates')
+app.config["SECRET_KEY"] = "sample"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
