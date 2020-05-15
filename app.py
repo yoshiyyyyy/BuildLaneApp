2797925afc5b4be8c71583ac517882e81558011f
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2020-05-15 17:27:16 +0900

    起動部分修正

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(debug=True)
