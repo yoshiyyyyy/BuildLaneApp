e34b1fd70a84197df6f73583c66421b9f0597e25
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2020-05-15 17:49:37 +0900

    ファイルアップロード実装

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from werkzeug.utils import secure_filename
 import requests
 import os
 import sys
-# sys.path.append("myspa")
-# import BuildLane
+sys.path.append("BuildLaneApp")
+import BuildLane
 
 
 
-UPLOAD_FOLDER = '/Users/yoshimasa/projects/myspa/backend/uploads/'
+UPLOAD_FOLDER = '/Users/yoshimasa/projects/python/BuildLaneApp/static/uploads/'
 
 app = Flask(__name__, template_folder='./templates')
 app.config["SECRET_KEY"] = "sample"
@@ -17,6 +17,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/", methods=["GET","POST"])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません', "failed")
+            return redirect(request.url)
+        else:
+            excel_row = request.form.get('excel_row') 
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            BuildLane.func(filename,int(excel_row))
+            return send_from_directory("./static/uploads", filename, as_attachment=True), os.remove('./static/uploads/%s' % filename)
 
 
 if __name__ == "__main__":
